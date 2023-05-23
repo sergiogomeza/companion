98ef84344ed9536bd0973992c9b147ee3ab44f5b
Sergio Gómez Arboleda <sergiogomez@drata.com>
2023-05-23 11:56:11 -0500

    delete create-react-app client

--- api/app.js
+++ api/app.js
@@ -1,41 +1,52 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// import * as url from "url";
+// const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const companion = require("@uppy/companion");
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const GOOGLE_DRIVE_KEY = process.env.GOOGLE_DRIVE_KEY;
+const GOOGLE_DRIVE_SECRET = process.env.GOOGLE_DRIVE_SECRET;
+const PORT = process.env.PORT;
+const FILE_PATH = path.join(__dirname, "output");
 
-var app = express();
+const companionOptions = {
+  providerOptions: {
+    drive: {
+      key: GOOGLE_DRIVE_KEY,
+      secret: GOOGLE_DRIVE_SECRET,
+    },
+  },
+  server: {
+    host: `localhost:${PORT}`,
+    protocol: "http",
+  },
+  filePath: FILE_PATH,
+  debug: true,
+  secret: "realsecret",
+  uploadUrls: `http://localhost:${PORT}`,
+};
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+  next();
 });
+app.use(bodyParser.json());
+app.use(
+  session({
+    secret: "realsecret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+const { app: companionApp } = companion.app(companionOptions);
+app.use(companionApp);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+const server = app.listen(PORT, () => {
+  console.log(`listening to port: ${PORT}`);
 });
-
-module.exports = app;
+companion.socket(server);
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+function customController(request, response) {
+  return response.status(200);
+}
 
-module.exports = router;
+router.get("/", customController);
+
+module.exports = { router };
